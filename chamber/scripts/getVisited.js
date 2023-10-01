8c6ab2b0f77f0a0697a3929c9121a25dc968952e
Tyrpak <greg@earthdawn.co.hu>
2023-10-01 08:26:33 +0200

    fix3

--- chamber/scripts/getVisited.js
+++ chamber/scripts/getVisited.js
@@ -1,24 +1,45 @@
 // milliseconds to days constant = 1000 ms/s * 60 s/m * 60 m/h * 24 h/day
 const msToDays = 84600000;
 // today's date
-const theDateToday = new Date();
+const today = new Date();
 
-// initialize display elements
-const lastVisited = document.querySelector("#visited");
+// store current date
+localStorage.setItem('lastVisitedTime', JSON.stringify(today));
 
-// processing
-const today = Date.now();
+//display last visited
 
+displayDaysSinceLastVisit();
 
-const christmasDate = new Date(Date.UTC(theDateToday.getFullYear(), 11, 25));
-// check if is the waing days of December, if so ... change to next year.
-if (theDateToday.getMonth() == 11 && theDateToday.getDate() > 25) {
-	christmasDate.setFullYear(christmasDate.getFullYear() + 1);
+function displayDaysSinceLastVisit() {
+  const visitsDisplay = document.querySelector('#visited');
+
+  const lastVisit = localStorage.getItem('lastVisitedTime');
+
+  if (!lastVisit) {
+    visitsDisplay.innerHTML = '<h2>Welcome! Let us know if you have any questions.</h2>';
+    
+    return;
+  }
+
+  const lastVisitDate = Date.parse(lastVisit);
+  
+  if (!lastVisitDate) {
+    // Stored date is not a valid format
+    return;
+  }
+
+  const currentDate = new Date();
+
+  const difference = currentDate - lastVisitDate;
+  const differenceInDays = Math.floor(difference / msToDays);
+
+  if (differenceInDays < 1) {
+	visitsDisplay.innerHTML = '<h2>Back so soon! Awesome!</h2>';
+	return;
+  }
+  else {
+	visitsDisplay.innerHTML = '<h2>You last visited " + differenceInDays + " days ago.</h2>' ;
+	return;
+  }
+ 
 }
-// find difference between epoch times in ms and convert to days
-let daysleft = (christmasDate.getTime() - Date.now()) / msToDays;
-
-todayElement.textContent = today;
-christmasElement.textContent = christmasDate.getTime();
-christmasDateElement.textContent = christmasDate;
-daysElement.textContent = `${daysleft.toFixed(0)} days`;
